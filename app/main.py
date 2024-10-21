3088f309ca6dc4ca334fb3f89fc21e56c79efafe
root <root@Patrick>
2024-10-21 19:29:37 -0400

    add endpoint

--- app/main.py
+++ app/main.py
@@ -10,8 +10,16 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Hello beautiful people"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/square/{a}")
+def sqr(a: int):
+    return {"square": a * a}
+
+@app.get("/multiply/{a}/{b}")
+def multiply(a: int, b: int):
+    return {"multiply": a * b}
