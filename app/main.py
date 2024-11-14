c0d541c23bb5a8adf28cf976aa459d4d6f318425
root <root@Patrick>
2024-11-13 22:55:27 -0500

    back to original

--- app/main.py
+++ app/main.py
@@ -1,56 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import mysql.connector
-import os
-import json
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-
-DBHOST = os.getenv('DBHOST')
-DBUSER = os.getenv('DBUSER')
-DBPASS = os.getenv('DBPASS')
-DB = os.getenv('DB')
-
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
+import json
+import os
 
 app = FastAPI()
 
-app.add_middleware(
-   CORSMiddleware,
-   allow_origins=["*"],
-   allow_methods=["*"],
-   allow_headers=["*"],
-)
+@app.get("/")  # zone apex
+def zone_apex():
+    return {"Hello": "Hello beautiful people"}
 
-@app.get('/genres')
-def get_genres():
-    query = "SELECT * FROM genres ORDER BY genreid;"
-    try:    
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        output = json_data
-        return(output)
-    except mysql.connector.Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
+@app.get("/add/{a}/{b}")
+def add(a: int, b: int):
+    return {"sum": a + b}
 
-@app.get('/songs')
-def get_songs():
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre = genres.genreid ORDER BY songs.title;"
-    try:
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        output = json_data
-        return(output)
-    except mysql.connector.Error as e:
-        return{"Error": "MySQL Error: " +  str(e)}
+@app.get("/square/{a}")
+def sqr(a: int):
+    return {"square": a * a}
+
+@app.get("/multiply/{a}/{b}")
+def multiply(a: int, b: int):
+    return {"multiply": a * b}
+
+@app.get("/subtract/{a}/{b}")
+def subtract(a: int, b: int):
+    return {"difference": a - b}
